4b423da8a0b228b208b6f2b25e57e45287c2649a
Jorn vii <157733779+Jornvii@users.noreply.github.com>
2024-11-27 16:31:16 +0700

    d9

--- lib/screens/report_page.dart
+++ lib/screens/report_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:intl/intl.dart';  // To format dates
-import 'package:path_provider/path_provider.dart';  // To get the local storage path
-import 'package:csv/csv.dart';  // For exporting data to CSV
+import 'package:intl/intl.dart'; // To format dates
+import 'package:path_provider/path_provider.dart'; // To get the local storage path
+import 'package:csv/csv.dart'; // For exporting data to CSV
 import 'dart:io';
 import '../models/transaction_db.dart';
 
@@ -19,6 +19,7 @@ class _ReportScreenState extends State<ReportScreen> {
   String selectedChartType = 'Doughnut';
   DateTime? startDate;
   DateTime? endDate;
+  bool isLoading = true; // Added a loading state
 
   @override
   void initState() {
@@ -26,34 +27,46 @@ class _ReportScreenState extends State<ReportScreen> {
     _loadData();
   }
 
-  Future<void> _loadData() async {
-    final db = TransactionDB();
-    final transactions = await db.getTransactions();
+Future<void> _loadData() async {
+  setState(() {
+    isLoading = true; // Show loading spinner
+  });
 
-    double income = 0;
-    double expense = 0;
+  final db = TransactionDB();
 
-    for (var transaction in transactions) {
-      if (transaction['isIncome'] == 1) {
-        income += transaction['amount'];
-      } else {
-        expense += transaction['amount'];
-      }
-    }
-
-    setState(() {
-      totalIncome = income;
-      totalExpense = expense;
-    });
+  // Fetch transactions based on the selected date range
+  List<Map<String, dynamic>> transactions;
+  if (startDate != null && endDate != null) {
+    transactions = await db.getTransactionsByDateRange(startDate!, endDate!);
+  } else {
+    transactions = await db.getTransactions(); // Fetch all transactions if no range is selected
   }
+
+  double income = 0;
+  double expense = 0;
+
+  for (var transaction in transactions) {
+    if (transaction['isIncome'] == 1) {
+      income += transaction['amount'];
+    } else {
+      expense += transaction['amount'];
+    }
+  }
+
+  setState(() {
+    totalIncome = income;
+    totalExpense = expense;
+    isLoading = false; // Data loading complete
+  });
+}
+
 
   Future<void> _selectDateRange() async {
     final selectedDateRange = await showDateRangePicker(
       context: context,
-      initialDateRange: DateTimeRange(
-        start: startDate ?? DateTime.now(),
-        end: endDate ?? DateTime.now(),
-      ),
+      initialDateRange: startDate != null && endDate != null
+          ? DateTimeRange(start: startDate!, end: endDate!)
+          : DateTimeRange(start: DateTime.now(), end: DateTime.now()),
       firstDate: DateTime(2000),
       lastDate: DateTime.now(),
     );
@@ -64,8 +77,8 @@ class _ReportScreenState extends State<ReportScreen> {
         endDate = selectedDateRange.end;
       });
 
-      // Reload data based on the selected date range
-      _loadData(); // You may want to filter your data based on the selected dates here
+      // Reload data filtered by date range if needed
+      _loadData(); // Add filtering logic if necessary
     }
   }
 
@@ -74,7 +87,7 @@ class _ReportScreenState extends State<ReportScreen> {
     final transactions = await db.getTransactions();
 
     List<List<String>> rows = [];
-    rows.add(['Date', 'Description', 'Amount', 'Type']);  // Headers
+    rows.add(['Date', 'Description', 'Amount', 'Type']); // Headers
 
     for (var transaction in transactions) {
       rows.add([
@@ -114,48 +127,53 @@ class _ReportScreenState extends State<ReportScreen> {
         ],
       ),
       body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-                side: const BorderSide(color: Colors.blueGrey, width: 0.5),
-              ),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * .75,
-                width: MediaQuery.of(context).size.width * .90,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        "Summary",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                          letterSpacing: 0.3,
-                        ),
+        child: isLoading
+            ? const CircularProgressIndicator() // Display loading indicator
+            : SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      side:
+                          const BorderSide(color: Colors.blueGrey, width: 0.5),
+                    ),
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * .75,
+                      width: MediaQuery.of(context).size.width * .90,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "Balance: \$${(totalIncome - totalExpense).toStringAsFixed(2)}",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: (totalIncome - totalExpense) < 0
+                                    ? Colors.red
+                                    : Colors.green,
+                                letterSpacing: 0.3,
+                              ),
+                            ),
+                          ),
+                          const Divider(
+                            color: Colors.grey,
+                            thickness: 0.3,
+                            endIndent: 10,
+                            indent: 10,
+                          ),
+                          _buildSummary(),
+                          const SizedBox(height: 20),
+                          _buildChartTypeSelector(),
+                          const SizedBox(height: 20),
+                          Expanded(child: _buildChart()),
+                        ],
                       ),
                     ),
-                    const Divider(
-                      color: Colors.grey,
-                      thickness: 0.3,
-                      endIndent: 10,
-                      indent: 10,
-                    ),
-                    _buildSummary(),
-                    const SizedBox(height: 20),
-                    _buildChartTypeSelector(),
-                    const SizedBox(height: 20),
-                    Expanded(child: _buildChart()),
-                  ],
+                  ),
                 ),
               ),
-            ),
-          ),
-        ),
       ),
     );
   }
@@ -169,7 +187,7 @@ class _ReportScreenState extends State<ReportScreen> {
           Column(
             children: [
               const Text(
-                "Total Income",
+                "Income",
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
               Text(
@@ -184,7 +202,7 @@ class _ReportScreenState extends State<ReportScreen> {
           Column(
             children: [
               const Text(
-                "Total Expense",
+                "Expense",
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
               Text(
@@ -212,7 +230,7 @@ class _ReportScreenState extends State<ReportScreen> {
       items: const [
         DropdownMenuItem(
           value: 'Doughnut',
-          child: Text('Doughnut Chart'),
+          child: Text('Donut Chart'),
         ),
         DropdownMenuItem(
           value: 'Line',
@@ -222,47 +240,46 @@ class _ReportScreenState extends State<ReportScreen> {
     );
   }
 
-Widget _buildChart() {
-  final data = [
-    ChartData(Colors.greenAccent, 'Income', totalIncome),
-    ChartData(Colors.redAccent, 'Expense', totalExpense),
-  ];
+  Widget _buildChart() {
+    final data = [
+      ChartData(Colors.greenAccent, 'Income', totalIncome),
+      ChartData(Colors.redAccent, 'Expense', totalExpense),
+    ];
 
-  if (selectedChartType == 'Line') {
-    return SfCartesianChart(
-      primaryXAxis: const CategoryAxis(),
-      title: const ChartTitle(text: 'Income vs Expense'),
-      legend: const Legend(isVisible: true),
-      series: <CartesianSeries>[
-        LineSeries<ChartData, String>(
-          dataSource: data,
-          xValueMapper: (ChartData data, _) => data.name,
-          yValueMapper: (ChartData data, _) => data.value,
-          color: Colors.blue,
+    if (selectedChartType == 'Line') {
+      return SfCartesianChart(
+        primaryXAxis: const CategoryAxis(),
+        title: const ChartTitle(text: 'Income vs Expense'),
+        legend: const Legend(isVisible: true),
+        series: <CartesianSeries>[
+          LineSeries<ChartData, String>(
+            dataSource: data,
+            xValueMapper: (ChartData data, _) => data.name,
+            yValueMapper: (ChartData data, _) => data.value,
+            color: Colors.blue,
+          ),
+        ],
+      );
+    } else {
+      return SfCircularChart(
+        legend: const Legend(
+          isVisible: true,
+          overflowMode: LegendItemOverflowMode.wrap,
+          position: LegendPosition.bottom,
         ),
-      ],
-    );
-  } else {
-    return SfCircularChart(
-      legend: const Legend(
-        isVisible: true,
-        overflowMode: LegendItemOverflowMode.wrap,
-        position: LegendPosition.bottom,
-      ),
-      series: <CircularSeries>[
-        DoughnutSeries<ChartData, String>(
-          dataSource: data,
-          pointColorMapper: (ChartData data, _) => data.color,
-          xValueMapper: (ChartData data, _) => data.name,
-          yValueMapper: (ChartData data, _) => data.value,
-          radius: '70%',
-          dataLabelSettings: const DataLabelSettings(isVisible: true),
-        ),
-      ],
-    );
+        series: <CircularSeries>[
+          DoughnutSeries<ChartData, String>(
+            dataSource: data,
+            pointColorMapper: (ChartData data, _) => data.color,
+            xValueMapper: (ChartData data, _) => data.name,
+            yValueMapper: (ChartData data, _) => data.value,
+            radius: '70%',
+            dataLabelSettings: const DataLabelSettings(isVisible: true),
+          ),
+        ],
+      );
+    }
   }
-}
-
 }
 
 class ChartData {
